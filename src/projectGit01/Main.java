3c79fe7dccfb7878d88e0485329492d2959920b2
Prabha97 <52439268+Prabha97@users.noreply.github.com>
2020-02-25 03:38:24 +0000

    Update Main.java

--- src/projectGit01/Main.java
+++ src/projectGit01/Main.java
@@ -2,6 +2,6 @@ package projectGit01;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("My first project under PAF");
+		System.out.println("My first project under PAF 0000000");
 	}
 }
